f237abf723b1354ab912907f96fb0c4cd50b70ef
Noemi Soares <142548973+noemisoares@users.noreply.github.com>
2025-08-20 20:09:22 +0000

    add

---
+++ mini_bio/app/page.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-gray-100">
+      <div className="max-w-md bg-white shadow-lg rounded-2xl p-6 text-center">
+        <img
+          src="/images/me.jpeg"
+          alt="Foto de perfil"
+          className="w-32 h-32 rounded-full mx-auto mb-4 shadow-md"
+        />
+
+        <h1 className="text-2xl font-bold mb-2">Noemi</h1>
+
+        {}
+        <p className="text-gray-700">oioi</p>
+        <p className="text-gray-700 mt-2">oioi</p>
+      </div>
+    </main>
+  );
+}
