b963c2bd0388ebdd7e2a7820df6c1366258cb8c6
Noemi Soares <142548973+noemisoares@users.noreply.github.com>
2025-08-21 22:27:01 +0000

    novo layout

--- mini_bio/app/page.js
+++ mini_bio/app/page.js
@@ -3,23 +3,22 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100 p-6">
-      <div className="flex gap-10 max-w-5xl items-center bg-white shadow-lg rounded-2xl p-8">
+      {}
+      <div className="flex items-center justify-center gap-12 max-w-5xl bg-white shadow-lg rounded-2xl p-10">
         {}
         <div className="flex-shrink-0">
           <Image
             src="/images/me.jpeg"
             alt="Minha Foto"
-            width={400}
-            height={400}
-            className="w-80 h-80 rounded-xl object-cover shadow-md"
+            width={350}
+            height={350}
+            className="rounded-xl object-cover shadow-md"
           />
         </div>
 
         {}
-        <div className="text-gray-900 max-w-lg">
-          <h1 className="text-4xl font-bold mb-6 text-center md:text-left">
-            NOEMI SOARES
-          </h1>
+        <div className="max-w-md text-gray-900">
+          <h1 className="text-4xl font-bold mb-6">NOEMI SOARES</h1>
 
           <p className="mb-4 leading-relaxed">
             Sou estudante do curso de Ciência da Computação da Universidade
@@ -32,8 +31,9 @@ export default function Home() {
             livros, jogando RPG de mesa e passando um tempo em família.
           </p>
 
-          <p className="italic text-gray-600 text-center md:text-left">
-            &quot;Trabalho duro é inútil para aqueles que não acreditam em si mesmos.&quot;
+          <p className="italic text-gray-600">
+            &quot;Trabalho duro é inútil para aqueles que não acreditam em si
+            mesmos.&quot;
           </p>
         </div>
       </div>
